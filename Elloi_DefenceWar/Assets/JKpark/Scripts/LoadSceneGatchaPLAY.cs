2757fb74f0511143326df7de6bbf965bbbcc028e
jungkeunpark <pjk2155@naver.com>
2023-08-04 17:36:58 +0900

    Feat.Contract Scene

---
+++ Elloi_DefenceWar/Assets/JKpark/Scripts/LoadSceneGatchaPLAY.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneGatchaPLAY : MonoBehaviour
+{
+    public void Onclick()
+    {
+        SceneManager.LoadScene("GatchaPlayScene");
+    }
+}
